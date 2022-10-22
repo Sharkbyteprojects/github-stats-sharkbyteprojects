eab09c2d41846ae24930bcfdd7dadb6444c21621
Sharkbyteprojects <sharkbyteprojects@protonmail.com>
2022-10-22 20:19:27 +0200

    Automated rehash

---
+++ integrityGenerator/updateThis.js
@@ -0,0 +1,36 @@
+const g = require("./mygen"),
+  fs = require("fs"),
+  { resolve } = require("path");
+
+(() => {
+  const jsonPath = [__dirname, "..", "docs", "framework", "blazor.boot.json"];
+
+  const path = resolve(...jsonPath);
+
+  if (!fs.existsSync(path)) {
+    console.error(`File \"${jsonPath}\" not found!`);
+    return;
+  }
+  try {
+    let jsd = JSON.parse(fs.readFileSync(path));
+    function doi(k, u, se) {
+      const _k = Object.keys(jsd.resources[k]);
+      let f = null;
+      for (let r of _k) {
+        f = g(u.concat(r), se);
+        console.log("Hash of file ".concat(r, " is ", f));
+        jsd.resources[k][f] = f;
+      }
+    }
+    doi(
+      "assembly",
+      "https://sharkbyteprojects.github.io/github-stats-sharkbyteprojects/framework/bin/",
+      0
+    );
+
+    fs.copyFileSync(path, path.concat(".old"));
+    fs.writeFileSync(path, Buffer.from(JSON.stringify(jsd), "utf8"));
+  } catch (ex) {
+    console.error("Error: ".concat(ex));
+  }
+})();
